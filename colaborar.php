a07f55129f58065cbb317b42db14bcb89aa1ea0a
mmserrano <mmserrano95@gmail.com>
2025-01-26 20:57:19 +0100

    encuesta google forms

--- colaborar.php
+++ colaborar.php
@@ -1,23 +1,32 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $collaboration = $_POST['collaboration'];
-    $other_collaboration = isset($_POST['other_collaboration']) ? $_POST['other_collaboration'] : '';
-    $hours = $_POST['hours'];
+require('PHPMailer/class.phpmailer.php');
+require('PHPMailer/class.smtp.php');
 
-    $to = 'ayudaciudadana.ia@gmail.com';
-    $subject = 'Nueva Colaboración';
-    $message = "Nombre: $name\nCorreo Electrónico: $email\nTeléfono: $phone\n¿Cómo deseas colaborar?: $collaboration\n$other_collaboration\nHoras semanales: $hours\nComentarios:%comments";
-    $headers = "From: $email";
+$mail = new PHPMailer(true);
 
-    if (mail($to, $subject, $message, $headers)) {
-        header('Location: gracias.html');
-        exit();
-    } else {
-        echo '<p>Algo ha fallado. Por favor, envía un correo directamente a <a href="mailto:ayudaciudadana.ia@gmail.com">ayudaciudadana.ia@gmail.com</a>.</p>';
-    }
-} else {
-    echo '<p>Método de solicitud no permitido.</p>';
+try {
+    // Configuración del servidor
+    $mail->IsSMTP();
+    $mail->SMTPAuth = true;
+    $mail->isHTML(true);
+    $mail->Host = "smtp.ayudaciudadana.info";
+    $mail->Username = "contacto@ayudaciudadana.info";
+    $mail->Password = "password"; 
+
+    // Remitente y destinatario
+    $mail->setFrom($email, 'Ayuda CiudaDANA');
+    $mail->addAddress('ayudaciudadana.ia@gmail.com');
+
+    // Contenido del correo
+    //$mail->isHTML(false);
+    $mail->Subject = 'Nueva Colaboración';
+    $mail->Body    = "Nombre: $name\nCorreo Electrónico: $email\nTeléfono: $phone\n¿Cómo deseas colaborar?: $collaboration\n$other_collaboration\nHoras semanales: $hours\nComentarios:$comments";
+    $mail->AltBody = "Nombre: $name\nCorreo Electrónico: $email\nTeléfono: $phone\n¿Cómo deseas colaborar?: $collaboration\n$other_collaboration\nHoras semanales: $hours\nComentarios:$comments";
+
+    $mail->send();
+    header('Location: gracias.html');
+    exit();
+} catch (Exception $e) {
+    echo '<p>Algo ha fallado. Por favor, envía un correo directamente a <a href="mailto:ayudaciudadana.ia@gmail.com">ayudaciudadana.ia@gmail.com</a>.</p>';
 }
 ?>
